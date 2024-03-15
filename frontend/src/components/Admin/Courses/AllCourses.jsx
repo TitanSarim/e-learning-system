e1ac0e58fa9c74e870f5ae230fe362d399fe411f
TitanSarim <muhammadsarim203@gmail.com>
2024-03-15 17:42:17 +0500

    sarim commit

--- frontend/src/components/Admin/Courses/AllCourses.jsx
+++ frontend/src/components/Admin/Courses/AllCourses.jsx
@@ -3,10 +3,14 @@ import SideBar from '../SideBar/SideBar'
 import NavBar from '../NavBar/NavBar'
 import { Link } from 'react-router-dom'
 import {AdminGetCourses, clearErrors} from '../../../actions/CoursesAction'
-
+import  moment from 'moment'
 import './AllCourses.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { toast } from 'react-toastify'
+import Loader from '../../Utils/Loader';
+import { TbTimeDuration10 } from "react-icons/tb";
+import { CgDollar } from "react-icons/cg";
+import { BsCalendar2Date } from "react-icons/bs";
 
 const AllCourses = () => {
 
@@ -47,14 +51,43 @@ const AllCourses = () => {
                 
                 <Link to={'/admin/create-course'}>Add Course</Link>
 
-                <div>
+                {loading ? <Loader/> : (
+                <div className='admin-allcourses-wrapper'>
 
                   {/* filters */}
                   <div>
-                    
+
+                  </div>
+
+                  {/* card */}
+                  <div className='admin-allcourses-cards'>
+                    {allCourses?.map((item) => (
+                      <div key={item.id} className='admin-allcourses-card-container'>
+
+                        <img src={JSON.parse(item.course_thumbnail).url} alt="Course Thumbnail" />
+                        <div className='admin-allcourses-card-header'>
+                          {item.status === 'active' ? <span className='admin-allcourses-card-header-status-acitve'>{item.status}</span> : <span className='admin-allcourses-card-header-status-inacitve'>{item.status}</span>}
+                          <p>{item.category}</p>
+                        </div>
+                        <p className='admin-allcourses-card-title'>{item.course_title}</p>
+                        <div className='admin-allcourses-card-length-price'>
+                          <p> 
+                              <TbTimeDuration10 size={25}/>
+                              <span>{item.timeline}</span> 
+                              Weeks
+                          </p>
+                          <p> {item.price}<CgDollar/></p>
+                        </div>
+                        <div className='admin-allcourses-card-footer'>
+                          <p><BsCalendar2Date size={25}/> {moment(item.updatedAt).format("MMM Do YY")}</p>
+                          <Link>View</Link>
+                        </div>
+                      </div>
+                    ))}
                   </div>
 
                 </div>
+                )}
 
             </div>
 
