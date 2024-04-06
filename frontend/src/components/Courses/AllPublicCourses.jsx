66ab7ed3cd2b29c572a7a9c63d26a109a436d8a3
Muhammad Azeem <mazeem182000@gmail.com>
2024-04-06 15:54:56 +0500

    Azeem commit

--- frontend/src/components/Courses/AllPublicCourses.jsx
+++ frontend/src/components/Courses/AllPublicCourses.jsx
@@ -1,114 +1,126 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import NavBar from '../NavBar/NavBar';
-import AllPublicCoursesFilters from './AllPublicCoursesFilters';
-import StarRatings from 'react-star-ratings'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
+import AllPublicCoursesFilters from "./AllPublicCoursesFilters";
+import StarRatings from "react-star-ratings";
 
 import { IoIosArrowForward } from "react-icons/io";
 import { CiCircleList } from "react-icons/ci";
 import { TbCategoryMinus } from "react-icons/tb";
-import Select from 'react-select';
+import Select from "react-select";
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
-import { useDispatch, useSelector } from 'react-redux'
-import Loader from '../Utils/Loader';
-import {PublicGetCourses, clearErrors} from '../../actions/CoursesAction'
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../Utils/Loader";
+import { PublicGetCourses, clearErrors } from "../../actions/CoursesAction";
 
-import './AllPublicCourses.css'
-import { toast } from 'react-toastify';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import "./AllPublicCourses.css";
+import { toast } from "react-toastify";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const selectPriceFilter = [
-  { value: 'ASC', label: 'Low To High' },
-  { value: 'DESC', label: 'High To Low' },
-  { value: '', label: 'None' },
+  { value: "ASC", label: "Low To High" },
+  { value: "DESC", label: "High To Low" },
+  { value: "", label: "None" },
 ];
 
-
 const AllPublicCourses = () => {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
+  const { Publiccourses, error, loading } = useSelector(
+    (state) => state.PublicCourse
+  );
 
-  const {Publiccourses, error, loading} = useSelector((state)=>state.PublicCourse);
-
-  const [courses, setCourses] = useState([])
-  const [pagination, setPagination] = useState([])
-  const [showDiv, setShowDiv] = useState('tbCategoryMinus');
-  const [filters, setFilters] = useState({price: ''});
+  const [courses, setCourses] = useState([]);
+  const [pagination, setPagination] = useState([]);
+  const [showDiv, setShowDiv] = useState("tbCategoryMinus");
+  const [filters, setFilters] = useState({ price: "" });
 
   const handleTbCategoryMinusClick = () => {
-      setShowDiv('tbCategoryMinus');
+    setShowDiv("tbCategoryMinus");
   };
 
   const handleCiCircleListClick = () => {
-      setShowDiv('ciCircleList');
+    setShowDiv("ciCircleList");
   };
 
   useEffect(() => {
-    if(error){
+    if (error) {
       toast.error(error);
       dispatch(clearErrors());
     }
-    dispatch(PublicGetCourses(1, filters))
-  }, [dispatch, error, filters])
+    dispatch(PublicGetCourses(1, filters));
+  }, [dispatch, error, filters]);
 
   useEffect(() => {
-    setCourses(Publiccourses)
-    setPagination(Publiccourses?.pagination)
-  }, [Publiccourses])
-
+    setCourses(Publiccourses);
+    setPagination(Publiccourses?.pagination);
+  }, [Publiccourses]);
 
   const renderPaginationLinks = () => {
     if (!pagination || pagination?.totalPages === 0) return null;
-  
+
     const links = [];
     const isFirstPage = pagination.currentPage === 1;
     const isLastPage = pagination.currentPage === pagination.totalPages;
-  
+
     const goToPreviousPage = () => {
       if (!isFirstPage) {
         const previousPage = pagination.currentPage - 1;
         dispatch(PublicGetCourses(previousPage));
       }
     };
-  
+
     const goToNextPage = () => {
       if (!isLastPage) {
         const nextPage = pagination.currentPage + 1;
         dispatch(PublicGetCourses(nextPage));
       }
     };
-  
+
     const startPage = Math.max(1, pagination.currentPage - 2);
     const endPage = Math.min(pagination.totalPages, pagination.currentPage + 2);
-  
+
     for (let i = startPage; i <= endPage; i++) {
       const pageNumber = i;
       links.push(
-        <Link key={i} to={`?page=${i}`} className={'pubic-all-courses-pagination-active'} onClick={() => dispatch(PublicGetCourses(pageNumber))}>
+        <Link
+          key={i}
+          to={`?page=${i}`}
+          className={"pubic-all-courses-pagination-active"}
+          onClick={() => dispatch(PublicGetCourses(pageNumber))}
+        >
           {i}
         </Link>
       );
     }
-  
+
     return (
       <>
         {isFirstPage ? (
-          <span className='pubic-all-courses-pagination-inActive'>
+          <span className="pubic-all-courses-pagination-inActive">
             <MdOutlineKeyboardArrowLeft />
           </span>
         ) : (
-          <Link to={`?page=${pagination.currentPage - 1}`} className='pubic-all-courses-pagination-active' onClick={goToPreviousPage}>
+          <Link
+            to={`?page=${pagination.currentPage - 1}`}
+            className="pubic-all-courses-pagination-active"
+            onClick={goToPreviousPage}
+          >
             <MdOutlineKeyboardArrowLeft />
           </Link>
         )}
         {links}
         {isLastPage ? (
-          <span className='pubic-all-courses-pagination-inActive'>
+          <span className="pubic-all-courses-pagination-inActive">
             <MdOutlineKeyboardArrowRight />
           </span>
         ) : (
-          <Link to={`?page=${pagination.currentPage + 1}`} className='pubic-all-courses-pagination-active' onClick={goToNextPage}>
+          <Link
+            to={`?page=${pagination.currentPage + 1}`}
+            className="pubic-all-courses-pagination-active"
+            onClick={goToNextPage}
+          >
             <MdOutlineKeyboardArrowRight />
           </Link>
         )}
@@ -124,79 +136,88 @@ const AllPublicCourses = () => {
     setFilters({ ...filters, price: selectedOption.value });
   };
 
-
   const customStyles = {
     control: (provided, state) => ({
       ...provided,
       width: 180,
       height: 50,
-      backgroundColor: '#311c5c',
-      border: state.isFocused ? '1px solid rgb(105, 105, 105)' : '1px solid rgb(105, 105, 105)',
+      backgroundColor: "#311c5c",
+      border: state.isFocused
+        ? "1px solid rgb(105, 105, 105)"
+        : "1px solid rgb(105, 105, 105)",
       outline: "none",
-      borderRadius: 4, 
-      cursour: "pointer"
-      }),
-      singleValue: (provided) => ({
-        ...provided,
-        color: 'white', 
-      }),  
-      placeholder: (provided) => ({
-        ...provided,
-        color: 'rgb(163, 163, 163)', 
-      }),
+      borderRadius: 4,
+      cursour: "pointer",
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: "white",
+    }),
+    placeholder: (provided) => ({
+      ...provided,
+      color: "rgb(163, 163, 163)",
+    }),
   };
 
   return (
-    <div className='pubic-all-courses'>
-      
-      <NavBar/>
-      
+    <div className="pubic-all-courses">
+      <NavBar />
+
       {/* banner */}
-      <div className='pubic-all-courses-banner'>
+      <div className="pubic-all-courses-banner">
         <div>
           <p>All Courses</p>
-          <span><Link to='/'>Home</Link> <IoIosArrowForward/> <p>Courses</p></span>
+          <span>
+            <Link to="/">Home</Link> <IoIosArrowForward /> <p>Courses</p>
+          </span>
         </div>
       </div>
 
-      <div className='pubic-all-courses-container'>
-        
+      <div className="pubic-all-courses-container">
         {/* filters */}
-        <AllPublicCoursesFilters applyFilters={applyFilters} courses={courses}/>
+        <AllPublicCoursesFilters
+          applyFilters={applyFilters}
+          courses={courses}
+        />
 
         {/* Content */}
-        {loading ? <div className='pubic-all-courses-loader'><Loader/></div> : (
-          <div className='pubic-all-courses-list'>
-
-            <div className='pubic-all-courses-list-style'>
+        {loading ? (
+          <div className="pubic-all-courses-loader">
+            <Loader />
+          </div>
+        ) : (
+          <div className="pubic-all-courses-list">
+            <div className="pubic-all-courses-list-style">
               <p>Showing {pagination?.totalCourses} Total Results</p>
 
-                <div>
-                  <Select
-                    defaultValue={selectPriceFilter.find(option => option.value === filters.price)}
-                    onChange={handlePriceFilterChange}
-                    options={selectPriceFilter}
-                    placeholder="Price"
-                    styles={customStyles}
-                  />
-                  <div onClick={handleTbCategoryMinusClick}>
-                    <TbCategoryMinus size={27}/>
-                  </div>
-                  <div onClick={handleCiCircleListClick}>
-                    <CiCircleList size={27} />
-                  </div>
+              <div>
+                <Select
+                  defaultValue={selectPriceFilter.find(
+                    (option) => option.value === filters.price
+                  )}
+                  onChange={handlePriceFilterChange}
+                  options={selectPriceFilter}
+                  placeholder="Price"
+                  styles={customStyles}
+                />
+                <div onClick={handleTbCategoryMinusClick}>
+                  <TbCategoryMinus size={27} />
                 </div>
+                <div onClick={handleCiCircleListClick}>
+                  <CiCircleList size={27} />
+                </div>
+              </div>
             </div>
 
-            {showDiv === 'tbCategoryMinus' && (
-              <div className='pubic-all-courses-boxes-view'>
+            {showDiv === "tbCategoryMinus" && (
+              <div className="pubic-all-courses-boxes-view">
                 {courses?.Publiccourses?.map((course) => (
-                  <div className='pubic-single-course-box-view' key={course.id}>
-                    <LazyLoadImage src={course.course_thumbnail} alt='Course'/>
-                    <div className='pubic-single-course-box-view-header'>
+                  <div className="pubic-single-course-box-view" key={course.id}>
+                    <LazyLoadImage src={course.course_thumbnail} alt="Course" />
+                    <div className="pubic-single-course-box-view-header">
                       <p>{course.category}</p>
                       <span>
-                        <StarRatings 
+                        <StarRatings
                           rating={JSON.parse(course.reviews)}
                           starDimension="20px"
                           starSpacing="2px"
@@ -206,68 +227,78 @@ const AllPublicCourses = () => {
                         ({JSON.parse(course.reviews)} Reviews)
                       </span>
                     </div>
-                      <p className='pubic-single-course-box-view-title'>{course.course_title}</p>
-                      <p className='pubic-single-course-box-view-teacher'><span>By </span> {course.teacher_name}</p>
-                    <div className='pubic-single-course-box-view-price'>
-                      <Link to={`/courses/course/${course.slug}`}>Enroll Now</Link>
+                    <p className="pubic-single-course-box-view-title">
+                      {course.course_title.slice(0, 40)}
+                    </p>
+                    <p className="pubic-single-course-box-view-teacher">
+                      <span>By </span> {course.teacher_name}
+                    </p>
+                    <div className="pubic-single-course-box-view-price">
+                      <Link to={`/courses/course/${course.slug}`}>
+                        Enroll Now
+                      </Link>
                       <p>${course.price}.00</p>
                     </div>
                   </div>
                 ))}
-                
               </div>
             )}
 
             {/* list view */}
-            {showDiv === 'ciCircleList' && (
-              <div className='pubic-all-courses-list-view'>
-              {courses?.Publiccourses?.map((course) => (
-                <div className='pubic-single-course-list-view' key={course.id}>
-                  <LazyLoadImage src={course.course_thumbnail} alt='Course'/>
+            {showDiv === "ciCircleList" && (
+              <div className="pubic-all-courses-list-view">
+                {courses?.Publiccourses?.map((course) => (
+                  <div
+                    className="pubic-single-course-list-view"
+                    key={course.id}
+                  >
+                    <LazyLoadImage src={course.course_thumbnail} alt="Course" />
 
-                  <div className='pubic-single-course-list-view-container'>
-
-                    <div className='pubic-single-course-list-view-header'>
-                      <p className='pubic-single-course-list-view-title'>{course.course_title}</p>
-                      <div>
-                        <p className='pubic-single-course-list-view-category'>{course.category}</p>
-                        <span>
-                          <StarRatings 
-                            rating={JSON.parse(course.reviews)}
-                            starDimension="20px"
-                            starSpacing="2px"
-                            numberOfStars={1}
-                            starRatedColor="#FFFF00"
-                          />
-                          ({JSON.parse(course.reviews)} Reviews)
-                        </span>
+                    <div className="pubic-single-course-list-view-container">
+                      <div className="pubic-single-course-list-view-header">
+                        <p className="pubic-single-course-list-view-title">
+                          {course.course_title}
+                        </p>
+                        <div>
+                          <p className="pubic-single-course-list-view-category">
+                            {course.category}
+                          </p>
+                          <span>
+                            <StarRatings
+                              rating={JSON.parse(course.reviews)}
+                              starDimension="20px"
+                              starSpacing="2px"
+                              numberOfStars={1}
+                              starRatedColor="#FFFF00"
+                            />
+                            ({JSON.parse(course.reviews)} Reviews)
+                          </span>
+                        </div>
+                        <p className="pubic-single-course-list-view-teacher">
+                          <span>By </span> {course.teacher_name}
+                        </p>
                       </div>
-                      <p className='pubic-single-course-list-view-teacher'><span>By </span> {course.teacher_name}</p>
-                    </div>
 
-                    <div className='pubic-single-course-list-view-price'>
-                      <Link to={`/courses/course/${course.slug}`}>Enroll Now</Link>
-                      <p>${course.price}.00</p>
+                      <div className="pubic-single-course-list-view-price">
+                        <Link to={`/courses/course/${course.slug}`}>
+                          Enroll Now
+                        </Link>
+                        <p>${course.price}.00</p>
+                      </div>
                     </div>
-
                   </div>
-
-                </div>
-              ))}
+                ))}
               </div>
             )}
 
-            <div className='pubic-all-courses-pagination'>
+            <div className="pubic-all-courses-pagination">
               {renderPaginationLinks()}
             </div>
-          
           </div>
         )}
-        
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default AllPublicCourses
+export default AllPublicCourses;
--- frontend/src/components/ProfileDetail/ProfileDetailSideBar.jsx
+++ frontend/src/components/ProfileDetail/ProfileDetailSideBar.jsx
@@ -1,117 +1,111 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import store from "../../Store";
-import {userLogOut } from "../../actions/UserActions";
+import { userLogOut } from "../../actions/UserActions";
 
-import './ProfileDetailSideBar.css'
+import "./ProfileDetailSideBar.css";
 
-const ProfileDetailSideBar = ({profileData}) => {
+const ProfileDetailSideBar = ({ profileData }) => {
   console.log(profileData);
 
-    const [completionRate, setCompletionRate] = useState(0);
+  const [completionRate, setCompletionRate] = useState(0);
 
-    const handleLogOut = () => {
-      store.dispatch(userLogOut());
-    }
+  const handleLogOut = () => {
+    store.dispatch(userLogOut());
+  };
 
-    useEffect(() => {
-      const calculateCompletionRate = () => {
-        const fields = [
-          'username',
-          'email',
-          'location',
-          'firstname',
-          'lastname',
-          'phoneno',
-          'Headline',
-          'about',
-          'education',
-          'skills',
-          'experience',
-          'social',
-          'cv',
-          'coverletter'
-        ];
-  
-        const fieldWeights = {
-          username: 5,
-          email: 5,
-          location: 10,
-          firstname: 5,
-          lastname: 5,
-          phoneno: 5,
-          Headline: 10,
-          about: 15,
-          education: 10,
-          skills: 5,
-          experience: 10,
-          social: 5,
-          cv: 5,
-          coverletter: 5
-        };
-  
-        let score = 0;
-        fields.forEach(field => {
-          if (field === 'education' || field === 'experience') {
-            if (profileData[field].length > 0) {
-              score += fieldWeights[field];
-            }
-          } else {
-            if (profileData[field]) {
-              score += fieldWeights[field];
-            }
-          }
-        });
-  
-        return score;
+  useEffect(() => {
+    const calculateCompletionRate = () => {
+      const fields = [
+        "username",
+        "email",
+        "location",
+        "firstname",
+        "lastname",
+        "phoneno",
+        "Headline",
+        "about",
+        "education",
+        "skills",
+        "experience",
+        "social",
+        "cv",
+        "coverletter",
+      ];
+
+      const fieldWeights = {
+        username: 5,
+        email: 5,
+        location: 10,
+        firstname: 5,
+        lastname: 5,
+        phoneno: 5,
+        Headline: 10,
+        about: 15,
+        education: 10,
+        skills: 5,
+        experience: 10,
+        social: 5,
+        cv: 5,
+        coverletter: 5,
       };
-  
-      const rate = calculateCompletionRate();
-      setCompletionRate(rate);
 
-      localStorage.setItem('completionRate', rate);
-    }, [profileData]);
+      let score = 0;
+      fields?.forEach((field) => {
+        if (field === "education" || field === "experience") {
+          if (profileData[field].length > 0) {
+            score += fieldWeights[field];
+          }
+        } else {
+          if (profileData[field]) {
+            score += fieldWeights[field];
+          }
+        }
+      });
 
-      
+      return score;
+    };
+
+    const rate = calculateCompletionRate();
+    setCompletionRate(rate);
+
+    localStorage.setItem("completionRate", rate);
+  }, [profileData]);
+
   return (
-    <div className='profile-detail-side-bar'>
-
-        <div className='profile-detail-side-bar-container'>
-        
-            <div className='profile-detail-side-bar-profile-completion'>
-                <p>Profile Completion:</p>
-                <div className='progress-bar'>
-                    <div className='progress' style={{ width: `${completionRate}%` }}>
-                        <p>{completionRate}%</p>
-                    </div>
-                </div>
+    <div className="profile-detail-side-bar">
+      <div className="profile-detail-side-bar-container">
+        <div className="profile-detail-side-bar-profile-completion">
+          <p>Profile Completion:</p>
+          <div className="progress-bar">
+            <div className="progress" style={{ width: `${completionRate}%` }}>
+              <p>{completionRate}%</p>
             </div>
-
-            <div className='profile-detail-side-bar-profile-settings'>
-                <div>
-                    <Link>Notifications</Link>
-                </div>
-
-                <div>
-                    <Link>Change Password</Link>
-                </div>
-                
-            </div>
-
-           <div className='profile-detail-side-bar-links'>
-                <div>
-                    <button onClick={handleLogOut}>Logout</button>
-                </div>
-
-                <div>
-                    <button>Delete My Account</button>
-                </div>
-           </div>
-
+          </div>
         </div>
 
-    </div>
-  )
-}
+        <div className="profile-detail-side-bar-profile-settings">
+          <div>
+            <Link>Notifications</Link>
+          </div>
 
-export default ProfileDetailSideBar
+          <div>
+            <Link>Change Password</Link>
+          </div>
+        </div>
+
+        <div className="profile-detail-side-bar-links">
+          <div>
+            <button onClick={handleLogOut}>Logout</button>
+          </div>
+
+          <div>
+            <button>Delete My Account</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileDetailSideBar;
